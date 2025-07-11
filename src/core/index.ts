2fc544be9fddb39f2bdc2df5b6213b9b3ae3e73d
radeqq007 <radeqq007@interia.pl>
2025-07-11 10:21:00 +0200

    chore: format code

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,3 @@
-export { $deepRef, $ref, $watch } from './reactivity.js';
 export type { DeepRef, Ref } from './reactivity.js';
+export { $deepRef, $ref, $watch } from './reactivity.js';
 export { $ } from './selector.js';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { $, $deepRef, $ref, $watch } from './core/index.js';
 export type { DeepRef, Ref } from './core/index.js';
+export { $, $deepRef, $ref, $watch } from './core/index.js';
