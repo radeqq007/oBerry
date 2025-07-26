c945faa39086dcdc801b70f485bd3c0030995874
radeqq007 <radeqq007@interia.pl>
2025-07-26 18:16:41 +0200

    feat: add function overload to selector function

--- src/core/selector/index.ts
+++ src/core/selector/index.ts
@@ -3,6 +3,9 @@ import { ElementWrapper } from '../wrapper';
 /**
  * Select DOM elements using a CSS selector, HTMLElement, NodeList, or an Array of HTMLElements.
  */
+export function $(selector: string): ElementWrapper;
+export function $(selector: NodeList): ElementWrapper;
+export function $(selector: HTMLElement[]): ElementWrapper;
 export function $(
   selector: string | HTMLElement | NodeList | HTMLElement[]
 ): ElementWrapper {
