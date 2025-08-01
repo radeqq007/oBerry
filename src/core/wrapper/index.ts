e5c826a65855ce78b8879c222f0673024bc104ea
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2025-08-01 20:14:19 +0200

    chore: format code

--- src/core/wrapper/index.ts
+++ src/core/wrapper/index.ts
@@ -669,7 +669,7 @@ export class ElementWrapper {
 
     if (typeof predicate === 'string') {
       // Filter by CSS selector
-      filteredElements = this.elements.filter(el => el.matches(predicate));
+      filteredElements = this.elements.filter((el) => el.matches(predicate));
     } else {
       // Filter by predicate function
       filteredElements = this.elements.filter((el, index) =>
