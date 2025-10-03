2c9e26703e4607fa86dd8b18b5ae7839405e56a2
Radosław Kaczmarczyk <radoslaw.kaczmarczyk@proton.me>
2025-10-03 08:51:23 +0200

    feat: add children arg into the $new function

--- src/core/creator/index.ts
+++ src/core/creator/index.ts
@@ -1,7 +1,12 @@
 import { ElementWrapper } from "../wrapper";
 
-export function $new(tag: string): ElementWrapper {
+export function $new(tag: string): ElementWrapper;
+export function $new(tag: string, children: ElementWrapper): ElementWrapper;
+export function $new(tag: string, children?: ElementWrapper): ElementWrapper {
   const el = document.createElement(tag);
   const wrapper = new ElementWrapper([el]);
+  if (children) {
+    wrapper.append(children);
+  }
   return wrapper;
 }
