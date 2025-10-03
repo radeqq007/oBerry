bd274934d14208def51a1d7367c5a325bcebb2d2
radeqq007 <radeqq007@interia.pl>
2025-10-03 21:38:38 +0200

    feat: add the option to append multiple children when creating new elements

--- src/core/creator/index.ts
+++ src/core/creator/index.ts
@@ -1,12 +1,12 @@
 import { ElementWrapper } from "../wrapper";
 
-export function $new(tag: string): ElementWrapper;
-export function $new(tag: string, children: ElementWrapper): ElementWrapper;
-export function $new(tag: string, children?: ElementWrapper): ElementWrapper {
+export function $new(tag: string, ...children: ElementWrapper[]): ElementWrapper {
   const el = document.createElement(tag);
   const wrapper = new ElementWrapper([el]);
-  if (children) {
-    wrapper.append(children);
+  
+  for (const child of children) {
+    wrapper.append(child);
   }
+  
   return wrapper;
 }
