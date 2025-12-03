6c2a035043e68c52ed9d3383bc97b4297aea87a4
radeqq007 <radeqq007@interia.pl>
2025-12-03 18:44:51 +0100

    feat: export https stuff

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,4 @@ export { $new } from "./creator";
 export { Plugin, use } from "./plugins";
 export { $computed, $effect, $effectScope, $ref } from "./reactivity";
 export { $ } from "./selector";
+export { $http, type FetchOptions, HttpError} from "./http"
