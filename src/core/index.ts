7373eaf68157f7f218df1552141e9591f0f44c6b
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-01-23 07:54:35 +0100

    feat: remove http helpers

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,4 +2,3 @@ export { $new } from "./creator";
 export { Plugin, use } from "./plugins";
 export { $computed, $effect, $effectScope, $ref } from "./reactivity";
 export { $ } from "./selector";
-export { $http, type FetchOptions, HttpError} from "./http"
