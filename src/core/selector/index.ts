08ad7600e4bcbc08f6ebab3ab163d52749f370ce
Radosław Kaczmarczyk <radeqq007@interia.pl>
2026-02-20 08:50:54 +0100

    refactor: improve selector logic

--- src/core/selector/index.ts
+++ src/core/selector/index.ts
@@ -1,21 +1,23 @@
 import { ElementWrapper } from "../wrapper";
 
-export function $<T extends HTMLElement>(selector: string): ElementWrapper;
-export function $<T extends HTMLElement>(selector: NodeList): ElementWrapper;
-export function $<T extends HTMLElement>(selector: HTMLElement): ElementWrapper;
-export function $<T extends HTMLElement>(selector: HTMLElement[]): ElementWrapper;
-export function $<T extends HTMLElement> (
-	selector: string | HTMLElement | NodeList | HTMLElement[],
-): ElementWrapper {
-	let elements: HTMLElement[] | NodeList = [];
+export function $<T extends HTMLElement = HTMLElement>(selector: string): ElementWrapper<T>;
+export function $<T extends HTMLElement = HTMLElement>(selector: NodeList): ElementWrapper<T>;
+export function $<T extends HTMLElement = HTMLElement>(selector: T): ElementWrapper<T>;
+export function $<T extends HTMLElement = HTMLElement>(selector: T[]): ElementWrapper<T>;
+export function $<T extends HTMLElement = HTMLElement> (
+	selector: string | T | NodeList | T[],
+): ElementWrapper<T> {
+	let elements: T[] = [];
+	
 	if (typeof selector === "string") {
-		elements = document.querySelectorAll(selector) as NodeListOf<T>;
+		elements = Array.from(document.querySelectorAll<T>(selector));
 	} else if (selector instanceof HTMLElement) {
-		elements = [selector] as T[];
-	} else if (selector instanceof NodeList || Array.isArray(selector)) {
-		elements = selector as NodeListOf<T> | T[];
+		elements = [selector as T];
+	} else if (selector instanceof NodeList) {
+		elements = Array.from(selector) as T[];
 	} else {
-		throw new Error("Invalid selector type");
+		elements = selector as T[]
 	}
-	return new ElementWrapper(elements);
+
+	return new ElementWrapper<T>(elements);
 }
